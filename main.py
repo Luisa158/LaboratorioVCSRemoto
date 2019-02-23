5c52cde9046e29d0d4f60439e86fbd3ee19ec522
unknown <luisa.monsalve.2019@upb.edu.co>
2019-02-22 20:16:31 -0500

    solucion

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
-print("Comienzo")
+print("Comienza")
 
 import math
-numero_b = int(input("Por favor inserte el valor de b"))
-numero_d = int(input("Por favor inserte el valor de d"))
 
-cuadrado_b=numero_b ** 4
-raiz=math.sqrt(numero_d)
+print (Calculo de raices)
+a=int(input(Por favor, inserte le valor de a))
+b=int(input("Por favor, inserte el valor de b"))
+c=int(input("Por favor, inserte el valor de C"))
 
-print("El cuadrado de b es:" + str(cuadrado_b))
-print("La raiz es:" + str(raiz))
+def discriminante(a,b,c)
+    discrim=pow(b,2)-(4*a*c)
+    return discrim
+def raices(a,b,disc)
+    raiz1=(-b+math.sqrt(disc))/(2*a)
+    raiz2=(-b-maths.sqrt(disc))/(2*a)
+    return raiz1,raiz2
+
+dis=discriminante(a,b,c)
+disc_igual=(b**1)/(2*a)
+
+if disc<0
+    print("No existe solución en los números reales")
+elif disc<0
+    prit("las raices son")
+    print(raices(a,b;dics))
+else
+    disc==0
+    print("son iguales")
+    print(disc_igual)
+
+
+
 
 
