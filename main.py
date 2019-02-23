82d4acd783eceb0de83d44f38b08b86c2839c0e2
unknown <luisa.monsalve.2019@upb.edu.co>
2019-02-22 20:28:59 -0500

    correcciones ortograficas

--- main.py
+++ main.py
@@ -7,23 +7,23 @@ a=int(input("Por favor, inserte le valor de a"))
 b=int(input("Por favor, inserte el valor de b"))
 c=int(input("Por favor, inserte el valor de C"))
 
-def discriminante(a,b,c)
+def discriminante(a, b, c):
     discrim=pow(b,2)-(4*a*c)
     return discrim
-def raices(a,b,disc)
+def raices(a,b,disc):
     raiz1=(-b+math.sqrt(disc))/(2*a)
     raiz2=(-b-maths.sqrt(disc))/(2*a)
     return raiz1,raiz2
 
-dis=discriminante(a,b,c)
+disc=discriminante(a,b,c)
 disc_igual=(b**1)/(2*a)
 
-if disc<0
+if disc<0:
     print("No existe solución en los números reales")
-elif disc<0
+elif disc<0:
     prit("las raices son")
-    print(raices(a,b;dics))
-else
+    print(raices(a,b,dics))
+else:
     disc==0
     print("son iguales")
     print(disc_igual)
