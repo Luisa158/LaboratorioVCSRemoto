1433154dd35ee853505286bf9e6d3b344c75c440
unknown <luisa.monsalve.2019@upb.edu.co>
2019-02-22 19:21:04 -0500

    mi primer commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Comienzo")
+
+numero_a = int(input("Por favor inserte el primer valor"))
+numero_c = int(input("Por favor inserte el segundo valor"))
+
+z= numero_a * numero_c
+y=numero_a ** 2
+
+print (z)
+print (y)
